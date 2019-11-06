e2df3a99bd18f9f71f51dc3bbc02a59015754372
annesel <annes@ut.ee>
2019-11-06 15:36:58 +0200

    Update requirements.R from editor

--- requirements.R
+++ requirements.R
@@ -2,7 +2,7 @@ library(remotes)
 
 ##### Install specific package versions from CRAN #####
 
-install_version("reshape2", "1.4.2")
+install_version("reshape2", "1.4.3")
 
 # install_version("ggplot2", "2.2.1")
 # install_version("dplyr", "0.7.4")
